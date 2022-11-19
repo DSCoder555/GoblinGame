9ed7ad26344b94cb46d30dca2bef7bc2a47772a9
DSCoder555 <99209431+DSCoder555@users.noreply.github.com>
2022-11-18 22:08:24 -0500

    11/18 work

---
+++ goblinGame.js
@@ -0,0 +1,130 @@
+var pile1 = 3;
+var pile2 = 3;
+var pile3 = 3;
+var isPlayerTurn = false;
+var gameOver = false;
+function start(){
+    pile1 = 3;
+    pile2 = 3;
+    pile3 = 3;
+    isPlayerTurn = false;
+    gameOver = false;
+    display();
+    document.getElementById("warning").innerHTML = "";
+    document.getElementById("action").innerHTML = "";
+}
+function takeStone(pileNum,count){
+    
+    if(pileNum == 1){
+        if(count > pile1){
+            return false;
+        }
+        else{
+            pile1 -= count;
+            return true;
+        }
+    }
+    else if(pileNum == 2){
+        if(count > pile2){
+            return false;
+        }
+        else{
+            pile2 -= count;
+            return true;
+        }
+    }
+    else{
+        if(count > pile3){
+            return false;
+        }
+        else{
+            pile3 -= count;
+            return true;
+        }
+    }
+}
+function goblinTurn(){
+    if(gameOver){
+        document.getElementById("warning").innerHTML = "The game is over";
+    }
+    else if(!isPlayerTurn){
+        if(pile1>=pile2 && pile1>=pile3){
+            takeStone(1,1);
+            document.getElementById("action").innerHTML = "The goblins removed 1 stone from stack 1";
+        }
+        else if(pile2>= pile3){
+            takeStone(2,1);
+            document.getElementById("action").innerHTML = "The goblins removed 1 stone from stack 2";
+        }
+        else{
+            takeStone(3,1);
+            document.getElementById("action").innerHTML = "The goblins removed 1 stone from stack 3";
+        }
+        isPlayerTurn = true;
+        display();
+        check();
+        if(gameOver){
+            document.getElementById("action").innerHTML = "The game is over.  You won, the goblins lost.";
+        }
+    }
+    else{
+        document.getElementById("warning").innerHTML = "It is your turn";
+    }
+}
+function display(){
+    lines = "";
+    for(i = 3; i > 0; i--){
+        if(pile1 >= i){
+            lines += "O";
+        }
+        else{
+            lines += "X";
+        }
+        lines += " ";
+        if(pile2 >= i){
+            lines += "O";
+        }
+        else{
+            lines += "X";
+        }
+        lines += " ";
+        if(pile3 >= i){
+            lines += "O";
+        }
+        else{
+            lines += "X";
+        }
+        lines += "<br>";
+    }
+    document.getElementById("display").innerHTML = lines;
+}
+function playerTurn(pileNum,count){
+    if(gameOver){
+        document.getElementById("warning").innerHTML = "The game is over";
+    }
+    else if(isPlayerTurn){
+        if(takeStone(pileNum,count)){
+            document.getElementById("warning").innerHTML = "";
+            display();
+            isPlayerTurn = false;
+            check();
+            if(gameOver){
+                document.getElementById("action").innerHTML = "The game is over.  You lost, the goblins won.";
+            }
+            else{
+                document.getElementById("action").innerHTML = "You removed " + count + " stone from stack " + pileNum;
+            }
+        }
+        else{
+            document.getElementById("warning").innerHTML = "That move is not possible";
+        }
+    }
+    else{
+        document.getElementById("warning").innerHTML = "It is not your turn.";
+    }
+}
+function check(){
+    if(pile1 == 0 && pile2 == 0 && pile3 == 0){
+        gameOver = true;
+    }
+}
